0ec933d02a999408685e471d4717a21e2288083c
engahmed10 <eng.computer1@yahoo.com>
2020-06-11 12:48:35 -0700

    added histories allready created to index for each mem

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,9 +1,16 @@
 class HistoriesController < ApplicationController
 
-get '/histories'  do
+get '/histories/index'  do
 
-
+      @histories=History.all.select{ |his| his.id == his.member_id }
     erb :'/histories/index'
 end
+
+get /histories/new
+
+
+
+
+
 
 end
