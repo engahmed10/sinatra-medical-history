d189eb571221bb36208cf6fafa675ebef11873f9
engahmed10 <eng.computer1@yahoo.com>
2020-06-16 11:56:39 -0700

    added delete and  edit for peocedures

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -36,26 +36,44 @@ class ProceduresController < ApplicationController
     redirect "/members/#{member.id}/procedures/#{procedure.id}"
   end
 
-  # GET: /procedures/5
   get "/members/:id/procedures/:id1" do
     @procedure = Procedure.find_by_id(params[:id1])
+    
     erb :"/procedures/show"
   end
 
-  # GET: /procedures/5/edit
   get "/members/:id/procedures/:id1/edit" do
-
-    
-    erb :"/procedures/edit"
+       @member=Member.find_by_id(params[:id])
+       @procedure = Procedure.find_by_id(params[:id1])
+       @diseases=Helpers.all_desease(session)
+       erb :"/procedures/edit"
   end
 
-  # PATCH: /procedures/5
   patch "/members/:id/procedures/:id1" do
-    redirect "/procedures/:id"
+
+    diseases=Helpers.all_desease(session)
+    member=Member.find_by_id(params[:id])
+    procedure=Procedure.find_by_id(params[:id1])
+    procedure.update(name:params[:name],desc:params[:desc],proce_doctor:params[:proce_doctor],
+    hospital:params[:hospital],date_porc:params[:date_porc],age_at_proc:params[:age_at_proc])
+
+    if params["disease"]["name"] != nil
+      disease=Disease.create(name:params["disease"]["name"],desc:params["disease"]["desc"])
+      procedure.disease =  disease
+    else
+      procedure.disease  = diseases.select{|dis| dis[procedure_id].to_i == dis.id }.first
+    end
+    procedure.save
+    member.procedures << procedure
+    flash[:message]="Successfully updated procedure"
+    redirect "/members/#{member.id}/procedures/#{procedure.id}"
+
   end
 
-  # DELETE: /procedures/5/delete
-  delete "/members/:id/procedures/:id1/delete" do
-    redirect "/procedures"
+  delete "/members/:id/procedures/:id1" do
+    member=Member.find_by_id(params[:id])
+    procedure=Procedure.find_by_id(params[:id1])
+    procedure.destroy
+    redirect "/members/#{member.id}/procedures"
   end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -2,6 +2,6 @@ class Member < ActiveRecord::Base
     belongs_to :user
     has_many :histories
     has_many :diseases, through: :history
-    has_many :procedure
+    has_many :procedures
     
 end
